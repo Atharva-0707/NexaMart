0951b46d916545dfeaa3d4658bf5c6908cdd0c4b
Atharva-0707 <atharvasrivastava2412@gmail.com>
2026-06-17 01:13:30 +0530

    feat: create user schema with authentication fields

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db'); 
+
 dotenv.config();
 connectDB();
 
@@ -11,6 +12,8 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.send("Server is running properly!");
 });
+
+app.use('/api/auth', require('./routes/authRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
