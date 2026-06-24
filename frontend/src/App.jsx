007148c1802cd07ecbed2b3c6031e5214b3aa43f
Atharva-0707 <atharvasrivastava2412@gmail.com>
2026-06-24 23:39:18 +0530

    feat: implement shop and cart navigation functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Shop from './pages/Shop'
+
+import Cart from './pages/Cart';
 import About from './pages/About'
 import Disclaimer from './pages/Disclaimer'
 import ReturnPolicy from './pages/ReturnPolicy'
@@ -13,7 +16,9 @@ function App() {
       <Navbar />
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
 
+            <Route path="/cart" element={<Cart />} />
             <Route path="/about" element={<About />} />
             <Route path="/disclaimer" element={<Disclaimer />} />
             <Route path="/return" element={<ReturnPolicy />} />
