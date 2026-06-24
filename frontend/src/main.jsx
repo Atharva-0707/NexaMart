5c048ee17551addac5c46e73c0ef9a46c2025ee2
Atharva-0707 <atharvasrivastava2412@gmail.com>
2026-06-24 19:23:51 +0530

    feat: implement frontend layout and state management setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { AuthProvider } from './context/authContext.jsx'
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import './styles/global.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Provider>
 )
