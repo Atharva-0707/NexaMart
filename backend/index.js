2ac14ab4a9e737e37b109ca345aa977aa966f153
Atharva-0707 <atharvasrivastava2412@gmail.com>
2026-06-25 23:00:58 +0530

    feat: add user profile and order confirmation pages

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 app.use(cors(
     {
-        origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+        origin: ['http://localhost:5173', 'http://127.0.0.1:5173', process.env.FRONTEND_URL],
         credentials: true
     }
 ));
